e917176ee0b756a801262f503612f5f70eb6f770
Christian Solis-Shepperson <solisshepperson.christian@gmail.com>
2018-05-19 18:52:33 -0400

    Added button to reveal side panel options

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Christian Solis-Shepperson on 5/19/18.
+//  Copyright © 2018 Christian Solis-Shepperson. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+
+}
