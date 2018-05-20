b2c14cb46820767a7008cecf5253a3caa4e2341f
Christian Solis-Shepperson <solisshepperson.christian@gmail.com>
2018-05-20 18:39:47 -0400

    Added ability to create accounts

---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,9 @@
+//
+//  AuthService.swift
+//  Smack
+//
+//  Created by Christian Solis-Shepperson on 5/20/18.
+//  Copyright © 2018 Christian Solis-Shepperson. All rights reserved.
+//
+
+import Foundation
---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Christian Solis-Shepperson on 5/20/18.
+//  Copyright © 2018 Christian Solis-Shepperson. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
