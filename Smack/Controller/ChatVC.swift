38eb00f480b68ab49593f639c0c2e0be4ba67d66
Christian Solis-Shepperson <solisshepperson.christian@gmail.com>
2018-06-02 10:40:12 -0400

    Added sockets to retrieve messages and channels in real time when created

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -8,15 +8,39 @@
 
 import UIKit
 
-class ChatVC: UIViewController {
+class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     //Outlets
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLbl: UILabel!
+    @IBOutlet weak var messageTxtBox: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    //Variables
+    var isTyping = false
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //will bind the keyboard to the bottom of the view,
+        //allowing user to see keyboard as they type and not overlap
+        view.bindToKeyboard()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+         sendBtn.isHidden = true
+        
+        //removes keyboad when pressed outside of the keyboard or text box
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
+        view.addGestureRecognizer(tap)
+        
         //using SWRevealController method to move the view controller as an overlay
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         
@@ -29,6 +53,17 @@ class ChatVC: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)), name: NOTIFY_USER_DATA_DID_CHANGE, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIFY_CHANNEL_SELECTED, object: nil)
         
+        SocketService.instance.getChatMessage { (success) in
+            if success{
+                self.tableView.reloadData()
+                
+                //scrolls to the bottom of the chat to see the most recent posting
+                if MessageService.instance.messages.count > 0 {
+                    let endIndex = IndexPath(row: MessageService.instance.messages.count - 1, section: 0)
+                    self.tableView.scrollToRow(at: endIndex, at: .bottom, animated: false)
+                }
+            }
+        }
         
         if AuthService.instance.isLoggedIn{
             AuthService.instance.findUserByEmail(completion: {(success) in
@@ -45,6 +80,7 @@ class ChatVC: UIViewController {
             onLoginGetMessages()
         } else {
             channelNameLbl.text = "Please Log In"
+            tableView.reloadData()
         }
     }
     
@@ -52,10 +88,42 @@ class ChatVC: UIViewController {
         updateWithChannel()
     }
     
+    @objc func handleTap(){
+        view.endEditing(true)
+    }
+    
     func updateWithChannel(){
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLbl.text = "#\(channelName)"
         getMessages()
+    }
+    
+    @IBAction func messageBoxEditing(_ sender: Any) {
+        if messageTxtBox.text == "" {
+            isTyping = false
+            sendBtn.isHidden = true
+        } else {
+            if  isTyping == false{
+                sendBtn.isHidden = false
+            }
+            isTyping = true
+        }
+    }
+    
+    
+    @IBAction func sendMsgPressed(_ sender: Any) {
+        if AuthService.instance.isLoggedIn{
+            guard let channelId = MessageService.instance.selectedChannel?.id else {return}
+            
+            guard let message = messageTxtBox.text else {return}
+            
+            SocketService.instance.addMesage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId) { (success) in
+                if success{
+                    self.messageTxtBox.text = ""
+                    self.messageTxtBox.resignFirstResponder()
+                }
+            }
+        }
     }
     
     func onLoginGetMessages(){
@@ -77,9 +145,28 @@ class ChatVC: UIViewController {
         
         MessageService.instance.findAllMessagesForChannel(channelID: channelID) { (success) in
             if success{
-                
+                self.tableView.reloadData()
             }
         }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as? MessageCell {
+            
+            let message = MessageService.instance.messages[indexPath.row]
+            cell.configureCell(message: message)
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.messages.count
     }
 
 }
--- Smack/Services/SocketService.swift
+++ Smack/Services/SocketService.swift
@@ -47,4 +47,34 @@ class SocketService: NSObject {
         }
     }
     
+    func addMesage(messageBody: String, userId:String, channelId:String, completion: @escaping CompletionHandler){
+        let user = UserDataService.instance
+        
+        socket.emit("newMessage", messageBody, userId, channelId, user.name, user.avatarName, user.avatarColor)
+        completion(true)
+        
+    }
+    
+    func getChatMessage(completion: @escaping CompletionHandler){
+        socket.on("messageCreated") { (dataArray, ack) in
+            guard let messageBody = dataArray[0] as? String else {return}
+            guard let channelID = dataArray[2] as? String else {return}
+            guard let userName = dataArray[3] as? String else {return}
+            guard let userAvatar = dataArray[4] as? String else {return}
+            guard let userAvatarColor = dataArray[5] as? String else {return}
+            guard let messageID = dataArray[6] as? String else {return}
+            guard let timeStamp = dataArray[7] as? String else {return}
+            
+            if channelID == MessageService.instance.selectedChannel?.id && AuthService.instance.isLoggedIn{
+                
+                let newMessage = Message(message: messageBody, userName: userName, channelID: channelID, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: messageID, timeStamp: timeStamp)
+                
+                MessageService.instance.messages.append(newMessage)
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+    
 }
